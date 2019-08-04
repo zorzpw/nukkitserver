ee6cbb96cbba856f13ddf552a38b22ac89708693
zorzpw <158380363@qq.com>
2019-08-04 21:59:35 +0800

    Add files via upload

--- backup.sh
+++ backup.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
+#！/bin/sh
+mkdir /home/beifen
+#创建一个临时文件（要保存备份的路径）
+cp -r /root/worlds/*  /home/beifen
+#数据存在backups目录下，备份到beifen目录下，所以先将数据拷过来
+tar -zcPvf /home/HiBIN.backup$(date +%Y%m%d.%T).tar.gz /home/beifen
+ #将数据所在文件夹beifen打包
+rm -rf /home/beifen/
+ #删除临时文件内容
+ 
+find ./ -mtime +30 -name "*.tar.gz" -exec rm -rf {} \;
+#删除改文件夹下超过30天的文件
 
-f_time=`date +%F_%H_%M_%S`
-
-if [ ! -d /bak ]
-then
-        mkdir /bak
-fi
-
-tar czf /bak/$f_time-log.tar.gz /root/*
-
-find /bak -mtime +7 -type f -name "*.tar.gz" -exec -rm -rf{} \;
+#HIBIN备份程序
